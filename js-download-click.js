afe8eeff581e3572e725a066937d4a829d3ccec5
mudassirali007 <mudassirali007@github.com>
2020-09-10 00:56:28 +0500

    push on git

---
+++ js-download-click.js
@@ -0,0 +1,15 @@
+(function(){
+window.download  = (url, filename) => {
+                var a = document.createElement('a');
+                a.href = url;
+                a.download = filename || 'download';
+                var clickHandler = () => {
+                    setTimeout(() => {
+                        a.removeEventListener('click', clickHandler);
+                    }, 150);
+                };
+                a.addEventListener('click', clickHandler, false);
+                a.click();
+                return a;
+            }
+})()
